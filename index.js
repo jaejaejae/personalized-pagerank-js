38a0aa16db11c5667dff112ab06671ebb665f9f3
jaejaejae <r.bespinyowong@gmail.com>
2017-10-31 13:22:02 +0800

    The first working version

--- index.js
+++ index.js
@@ -1,11 +1,113 @@
 'use strict';
 
 /**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
+ * Computing PageRank
+ * @param {list of lists of integers} adjacencyList
+ * @param {dict} personalised A dictionary containing the personalised probability of the node. Not indicating means 0. The values are normalized to 1.
+ * @param {number} damping The probability of following the links.
+ * @param {integer} maxIteration The maximum number of iterations in running the algorithm to prevent the code from running forever.
+ * @param {function} callback A callback function with parameter list of pagerank values.
+ * @return {numbers} A list of PageRank scores of which sum is equal to 1.
  */
-module.exports = function (number, locale) {
-  return number.toLocaleString(locale);
+module.exports = function (adjacencyList, damping = 0.85, personalised = undefined, maxIteration = 1000, threshold = 0.001, callback = undefined) {
+  var pagerank = getPageRank(adjacencyList, personalised, damping, maxIteration, threshold);
+  if (callback) {
+    callback(pagerank);
+  }
+  return pagerank;
 };
+
+const rangeGen = N => [...(function * () {
+    let i = 0;
+    while (i < N) 
+      yield i++
+    }
+  )()];
+
+function getPageRank(adjacencyList, personalised, damping, maxIteration, threshold) {
+  validateInput(adjacencyList, personalised, damping, maxIteration, threshold);
+  var iteration = 1;
+  var totalNodes = adjacencyList.length;
+  var pagerankScores = new Array(totalNodes).fill(1);
+  var pagerankSum = 1.0 * totalNodes;
+  const allNodes = rangeGen(totalNodes);
+  while (iteration < maxIteration) {
+    var error = 0.0;
+    ++iteration;
+    var newPagerankScores = new Array(totalNodes).fill(0);
+    for (let u = 0; u < totalNodes; ++u) {
+      if (personalised === undefined) {
+        newPagerankScores[u] += (1 - damping) * 1.0 / totalNodes;
+      } else if (u in personalised) {
+        newPagerankScores[u] += (1 - damping) * personalised[u];
+      }
+      let neighbours = adjacencyList[u];
+      if (neighbours.length == 0) {
+        neighbours = allNodes;
+      }
+      for (let v of neighbours) {
+        newPagerankScores[v] += 1.0 * damping * pagerankScores[u] / neighbours.length;
+      }
+    }
+    pagerankSum = 0.0;
+    for (let u = 0; u < totalNodes; ++u) {
+      pagerankSum += newPagerankScores[u];
+      error = Math.max(error, Math.abs(newPagerankScores[u] - pagerankScores[u]));
+    }
+    pagerankScores = newPagerankScores;
+    if (error < threshold) {
+      break;
+    }
+  }
+
+  for (let u = 0; u < totalNodes; ++u) {
+    pagerankScores[u] /= pagerankSum;
+  }
+
+  return pagerankScores;
+}
+
+function initPagerankScores(adjacencyList) {
+  return new Array(adjacencyList.length).fill(1);
+}
+
+function validateInput(adjacencyList, personalised, damping, maxIteration, threshold) {
+  if (maxIteration <= 0) {
+    throw "The maximum number of iterations must be greater than 0";
+  }
+  if (!Number.isInteger(maxIteration)) {
+    throw "The maximum number of iterations should be an integer";
+  }
+  if (threshold <= 0 || threshold >= 1) {
+    throw "The error threshold must be between 0 and 1 (exclusive)."
+  }
+  if (damping < 0 || damping > 1) {
+    throw "The damping factor must be between 0 and 1 (inclusive)."
+  }
+
+  var totalNodes = adjacencyList.length;
+  for (let ls of adjacencyList) {
+    for (let i of ls) {
+      if (i < 0 || i >= totalNodes) {
+        throw `The adjacencylist can only have nodes with index 0 to ${totalNodes - 1}.`
+      }
+    }
+  }
+
+  if (personalised) {
+    var sumP = 0.0;
+    for (let p of Object.keys(personalised)) {
+      if (p < 0 || p >= totalNodes) {
+        throw `The personalised node must be an integer from 0 to ${totalNodes - 1} not ${p}.`
+      }
+      sumP += personalised[p];
+    }
+    if (sumP > 1.0) {
+      console.warn(`Summation of personalised vector (${sumP}) is greater than 1.`);
+      for (let p of Object.keys(personalised)) {
+        personalised[p] /= sumP;
+      }
+    }
+  }
+  return true;
+}
--- test/test.js
+++ test/test.js
@@ -1,62 +1,183 @@
 'use strict';
 
 var expect = require('chai').expect;
-var numFormatter = require('../index');
+var assert = require('chai').assert;
+var personalizedPageRank = require('../index');
 
-describe('#numFormatter', function () {
-  it('should convert single digits', function () {
-    var result = numFormatter(1);
-    expect(result)
+function checkFloatArray(ls, expectedLs, delta) {
+  assert(ls.length == expectedLs.length);
+  for (let i = 0; i < ls.length; ++i) {
+    expect(ls[i])
       .to
-      .equal('1');
+      .be
+      .closeTo(expectedLs[i], delta);
+  }
+}
+
+function checkSumArray(ls, expectedSum, delta) {
+  let sum = 0;
+  for (let l of ls) {
+    sum += l;
+  }
+  expect(sum)
+    .to
+    .be
+    .closeTo(expectedSum, delta);
+}
+
+describe('#personalizedPageRank', function () {
+  var cyclicGraph;
+  var connectedGraph;
+  var danglingGraph;
+  const threshold = 0.01;
+
+  before(function () {
+    /**
+     * Adjacency Matrix Declaration
+     * */
+    // cyclicGraph = [   [     0, 1, 0   ],   [     0, 0, 1   ],   [1, 0, 0] ];
+    // connectedGraph = [   [     0, 1, 0, 1   ],   [ 0, 0, 0, 1   ],   [     1, 0,
+    // 0, 0   ],   [0, 0, 1, 0] ]; danglingGraph = [ [ 0, 1, 0, 0   ],   [     0, 0,
+    // 1, 0   ],   [     1, 0, 0, 1   ],   [0, 0, 0, 0] ];
+
+    /**
+     * Adjacency List Declaration
+     */
+    cyclicGraph = [[1], [2], [0]
+    ];
+    connectedGraph = [
+      [
+        1, 3
+      ],
+      [3],
+      [0],
+      [2]
+    ];
+    danglingGraph = [
+      [1],
+      [2],
+      [
+        0, 3
+      ],
+      [] // This is a dangling node.
+    ];
   });
 
-  it('should convert double digits', function () {
-    var result = numFormatter(12);
-    expect(result)
-      .to
-      .equal('12');
+  describe('pagerank validation', function () {
+    it('should normalise the personalised vector to 1', function () {
+      var personalisedVector = {
+        0: 1.0,
+        1: 1.0,
+        2: 2.0
+      };
+      var normalizedPersonalisedVector = {
+        0: 0.25,
+        1: 0.25,
+        2: 0.5
+      };
+      var scores = personalizedPageRank(cyclicGraph, 0.85, personalisedVector, 2, 0.5);
+      expect(personalisedVector)
+        .to
+        .deep
+        .equal(normalizedPersonalisedVector);
+    });
+
+    it('should not allow invalid out-of-index adjacency list', function () {
+      expect(() => personalizedPageRank([
+        [1]
+      ], 0.85))
+        .to
+        .throw();
+    });
+
+    it('should not allow invalid out-of-index preference vector', function () {
+      expect(() => personalizedPageRank([[1], [0]
+      ], 0.85, {2: 0.8}))
+        .to
+        .throw();
+    });
+
+    it('should only allow damping factor to be 0 from 1', function () {
+      expect(() => personalizedPageRank(cyclicGraph, -0.1))
+        .to
+        .throw();
+      expect(() => personalizedPageRank(cyclicGraph, 1.1))
+        .to
+        .throw();
+    });
+
+    it('should only allow positive integer maximum number of iterations', function () {
+      expect(() => personalizedPageRank(cyclicGraph, 0.85, undefined, 1.1))
+        .to
+        .throw();
+      expect(() => personalizedPageRank(cyclicGraph, 0.85, undefined, -2))
+        .to
+        .throw();
+    });
   });
 
-  it('should convert triple digits', function () {
-    var result = numFormatter(123);
-    expect(result)
-      .to
-      .equal('123');
+  describe('pagerank without personalization', function () {
+    it('should give equal score on cyclic graph', function () {
+      const scores = personalizedPageRank(cyclicGraph, 0.85, undefined, 200, threshold, undefined);
+      const totalNodes = cyclicGraph.length;
+      const expectedScores = new Array(totalNodes).fill(1.0 / totalNodes);
+      checkSumArray(scores, 1, threshold * 10);
+      checkFloatArray(scores, expectedScores, threshold * 10);
+    });
+
+    it('should give correct PageRank values in the normal graph', function () {
+      const scores = personalizedPageRank(connectedGraph, 0.85, undefined, 100, threshold, undefined);
+      const expectedScores = [0.27643143604609594, 0.153577084051318, 0.28239539717581963, 0.28759608272676634];
+      checkSumArray(scores, 1, threshold * 10);
+      checkFloatArray(scores, expectedScores, threshold * 10);
+    });
+
+    it('should be able to deal with dangling nodes', function () {
+      const scores = personalizedPageRank(danglingGraph, 0.85, undefined, 100, threshold * 0.1, undefined);
+      const expectedScores = [0.21370906701224518, 0.2645985423017749, 0.30798332367373477, 0.21370906701224518];
+      checkSumArray(scores, 1, threshold * 10);
+      checkFloatArray(scores, expectedScores, threshold * 10);
+    });
   });
 
-  it('should convert 4 digits', function () {
-    var result = numFormatter(1234);
-    expect(result)
-      .to
-      .equal('1,234');
-  });
+  describe('pagerank with personalization', function () {
+    it('should give equal score on cyclic graph', function () {
+      const personalised = {
+        0: 0.8,
+        1: 0.1,
+        2: 0.1
+      };
+      const scores = personalizedPageRank(cyclicGraph, 0.85, personalised, 200, threshold, undefined);
+      const totalNodes = cyclicGraph.length;
+      const expectedScores = [0.3664730256020338, 0.33158913900340176, 0.3019378353945645];
+      checkSumArray(scores, 1, threshold * 10);
+      checkFloatArray(scores, expectedScores, threshold * 10);
+    });
 
-  it('should convert 5 digits', function () {
-    var result = numFormatter(12345);
-    expect(result)
-      .to
-      .equal('12,345');
-  });
+    it('should give correct PageRank values in the normal graph', function () {
+      const personalised = {
+        0: 0.8,
+        1: 0.1,
+        2: 0.1
+      };
+      const scores = personalizedPageRank(connectedGraph, 0.85, personalised, 200, threshold, undefined);
+      const totalNodes = cyclicGraph.length;
+      const expectedScores = [0.32233111368965606, 0.1531815533614323, 0.2508555395130727, 0.27363179343583904];
+      checkSumArray(scores, 1, threshold * 10);
+      checkFloatArray(scores, expectedScores, threshold * 10);
+    });
 
-  it('should convert 6 digits', function () {
-    var result = numFormatter(123456);
-    expect(result)
-      .to
-      .equal('123,456');
-  });
-
-  it('should convert 7 digits', function () {
-    var result = numFormatter(1234567);
-    expect(result)
-      .to
-      .equal('1,234,567');
-  });
-
-  it('should convert 8 digits', function () {
-    var result = numFormatter(12345678);
-    expect(result)
-      .to
-      .equal('12,345,678');
+    it('should be able to deal with dangling nodes', function () {
+      const personalised = {
+        0: 0.8,
+        1: 0.1,
+        2: 0.1
+      };
+      const scores = personalizedPageRank(danglingGraph, 0.85, personalised, 200, threshold, undefined);
+      const totalNodes = cyclicGraph.length;
+      const expectedScores = [0.2663339437160614, 0.2792345393049101, 0.29146215683406046, 0.16296936014496802];
+      checkSumArray(scores, 1, threshold * 10);
+      checkFloatArray(scores, expectedScores, threshold * 10);
+    });
   });
 });
